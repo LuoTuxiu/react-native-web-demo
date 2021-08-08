71752f8a632c63c00f150130a9783778821237f0
Luotuxiu <359703836@qq.com>
2021-08-08 17:42:45 +0800

    feat: init project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './index.web.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+    },
+  },
+};
